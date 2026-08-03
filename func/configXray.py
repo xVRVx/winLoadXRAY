272bd3be7045c7cc198fd2af6722c925c2483fc4
xVRVx <63975481+xVRVx@users.noreply.github.com>
2026-08-03 14:38:43 +0300

    Add files via upload

--- func/configXray.py
+++ func/configXray.py
@@ -50,17 +50,8 @@ def generate_config(data):
                         "geosite:category-ip-geo-detect",
                         "geosite:apple",
                         "geosite:apple-pki",
-                        "geosite:huawei",
-                        "geosite:xiaomi",
-                        "geosite:category-android-app-download",
-                        "geosite:f-droid",
                         "geosite:yandex",
                         "geosite:vk",
-                        "geosite:microsoft",
-                        "geosite:win-update",
-                        "geosite:win-extra",
-                        "geosite:google-play",
-                        "geosite:steam",
                         "geosite:category-ru"
                     ],
                     "outboundTag": "direct"
